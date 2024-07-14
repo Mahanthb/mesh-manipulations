792d5dec429cb8bae6f90bf2ccec09fa3f47048d
Mahanthb <142413322+Mahanthb@users.noreply.github.com>
2024-07-14 19:51:57 +0530

    Update CardComponent.js

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -11,16 +11,20 @@ const CardComponent = ({
   toggleAnimation,
   isAnimationPlaying,
   loading,
+  animations,
+  handleAnimationSelect,
+  selectedAnimation,
 }) => {
   return (
     <div className="card">
-        <h2>Import</h2>
+      <h2>Import</h2>
       <input
         type="file"
+        accept=".glb,.gltf"
         onChange={handleFileUpload}
         style={{ fontSize: '18px', padding: '10px', marginBottom: '20px', width: '100%' }}
       />
-
+      
       <select
         value={selectedFile ? selectedFile.name : ''}
         onChange={handleSelectChange}
@@ -31,25 +35,38 @@ const CardComponent = ({
           <option key={file.name} value={file.name}>{file.name}</option>
         ))}
       </select>
-
-      <br />
+      
       <h2>Export</h2>
-
       <button onClick={handleExportToLocal} style={{ fontSize: '18px', padding: '10px', marginBottom: '20px' }} title="Export to Local">
         <FaDownload style={{ marginRight: '8px' }} />
+        Export to Local
       </button>
-
+      
       <button onClick={handleExportToFirebase} style={{ fontSize: '18px', padding: '10px', marginBottom: '20px' }} title="Export to Firebase">
         <FaCloudUploadAlt style={{ marginRight: '8px' }} />
+        Export to Firebase
       </button>
-      <br></br>
+      
       <h2>Animations</h2>
-
       <button onClick={toggleAnimation} style={{ fontSize: '18px', padding: '10px', marginBottom: '20px' }} title={isAnimationPlaying ? "Pause Animation" : "Play Animation"}>
         {isAnimationPlaying ? <FaPause style={{ marginRight: '8px' }} /> : <FaPlay style={{ marginRight: '8px' }} />}
+        {isAnimationPlaying ? "Pause Animation" : "Play Animation"}
       </button>
-
-      {loading && <p style={{ color: 'black', fontSize: '18px' }}>Loading...</p>}
+      
+      {animations && animations.length > 0 && (
+        <select
+          value={selectedAnimation || ''}
+          onChange={handleAnimationSelect}
+          style={{ fontSize: '18px', padding: '10px', marginBottom: '20px', width: '100%', color: 'black', backgroundColor: '#61dafb', cursor: 'pointer' }}
+        >
+          <option value="">Select Animation</option>
+          {animations.map(animation => (
+            <option key={animation.name} value={animation.name}>{animation.name}</option>
+          ))}
+        </select>
+      )}
+      
+      {loading && <p>Loading...</p>}
     </div>
   );
 };
